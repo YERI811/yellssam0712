283e8606379dabe3bb1438027c1b2442c06f7416
YERI811 <yellkim0118@gmail.com>
2025-07-12 16:04:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<p><strong>👥 조 이름:</strong>
+  <input type="text" id="groupName" placeholder="조 이름을 입력하세요" oninput="saveInputs()" />
+</p>
+
+<p><strong>조원 이름:</strong>
+  <input type="text" id="groupMembers" placeholder="조원 이름을 입력하세요" oninput="saveInputs()" />
+</p>
